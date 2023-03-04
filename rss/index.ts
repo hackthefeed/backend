1e1cec4d8ca3765ff1bb1695cb37bc8b80c864e1
matteopolak <matteopolak@hotmail.com>
2023-03-03 23:08:33 -0500

    fix: tsconfig

--- rss/index.ts
+++ rss/index.ts
@@ -2,6 +2,28 @@
 // run every few minutes
 // add function to add new rss feeds to database
 
-export function scrape() {
-	
+import axios from 'axios';
+import { Producer } from '@prisma/client';
+import { prisma } from '../database';
+
+export function getActiveFeeds(connected: number[]) {
+	const feeds = prisma.producer.findMany({
+		where: {
+			subscribers: {
+				some: {
+					id: {
+						in: connected
+					}
+				}
+			}
+		}
+	});
+
+	return feeds;
+}
+
+export async function updateFeed(feed: Producer) {
+	const data = await axios.get(feed.feedUrl);
+
+	console.log(data);
 }
---
+++ rss/migrate.ts
@@ -0,0 +1,24 @@
+import fs from 'fs/promises';
+import { prisma } from '../database';
+
+type RawFeed = {
+	name: string;
+	url: string;
+}
+
+async function main() {
+	const feed: RawFeed[] = JSON.parse(await fs.readFile('./feeds.json', 'utf8'));
+
+	const response = await prisma.producer.createMany({
+		skipDuplicates: true,
+		data: feed.map((f) => ({
+			name: f.name,
+			feedUrl: f.url,
+			website: f.url,
+		}))
+	});
+
+	console.log(`Created ${response.count} feeds.`);
+}
+
+main();
